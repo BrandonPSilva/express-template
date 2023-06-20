44ef9e413df28fd67e2169fcde4f749617f149a8
BrandonPSilva <brandon@bsilva.dev>
2023-06-19 22:14:06 -0400

    /Download

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express')
 var router = express.Router()
+var resourcesPath = `${__dirname}/../resources/` // Path to the resources folder where files served will be stored.
 
-/* GET home page. */
+// Home Page
 router.get('/', function (req, res, next) {
     res.send('test')
+})
+
+// Download
+router.get('/download', function (req, res) {
+    var fileName = 'test.txt'
+    res.download(resourcesPath + fileName)
 })
 
 module.exports = router
