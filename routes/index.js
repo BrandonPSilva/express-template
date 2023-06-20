6b89f4e5403f6a35a0a7bc554e485639934dca65
BrandonPSilva <brandon@bsilva.dev>
2023-06-19 23:17:36 -0400

    Added API Key auth for download

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,28 @@
 var express = require('express')
 var router = express.Router()
+require('dotenv').config()
 var resourcesPath = `${__dirname}/../resources/` // Path to the resources folder where files served will be stored.
+
+var invalidKey = 'Invalid API Key'
 
 // Home Page
 router.get('/', function (req, res, next) {
     res.send('test')
 })
 
-// Download
+// Download without key
 router.get('/download', function (req, res) {
-    var fileName = 'test.txt'
-    res.download(resourcesPath + fileName)
+    res.send(invalidKey)
+})
+
+// Download
+router.get('/download/:id', function (req, res) {
+    if (req.params.id == process.env.API_KEY){
+        var fileName = 'test.txt'
+        res.download(resourcesPath + fileName)
+    } else {
+        res.send(invalidKey)
+    }
 })
 
 module.exports = router
